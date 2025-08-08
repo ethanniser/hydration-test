9ad349e561b5461d109c218a73814e7baa2b2941
Ethan Niser <ethan@niser.com>
2025-08-07 20:47:40 -0700

    1

---
+++ src/app/experiment.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+interface PreArgs {
+  listenUntilHydrated: (
+    element: HTMLElement,
+    event: string,
+    callback: (e: Event) => void
+  ) => void;
+}
+
+declare const $useState: typeof useState;
+declare const $ref: typeof useRef;
+declare const $pre: (fn: (args: PreArgs) => void) => void;
+
+export default function Client() {
+  return (
+    <>
+      <Clock />
+      <Input />
+    </>
+  );
+}
+
+// a way to define a initial server state
+// a way to define a fn for deriving before interactive state on the client
+// a way to set the elements that depend on that state
+// a way for react to gracefully initialize with the state on the client
+
+function Clock() {
+  "use before interactive";
+
+  const [time, setTime] = $useState(new Date());
+  const secondHand = $ref<SVGLineElement>(null);
+
+  const secondRotation = time.getSeconds() * 6 + time.getMilliseconds() * 0.006;
+
+  $pre(() => {
+    if (secondHand.current) {
+      secondHand.current.setAttribute(
+        "transform",
+        `rotate(${secondRotation}, 50, 50)`
+      );
+    }
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 10);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div>
+      <svg width="200" height="200" viewBox="0 0 100 100">
+        <circle
+          cx="50"
+          cy="50"
+          r="45"
+          fill="white"
+          stroke="black"
+          strokeWidth="2"
+        />
+
+        <circle cx="50" cy="50" r="2" fill="black" />
+
+        <line
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="10"
+          stroke="red"
+          strokeWidth="1"
+          transform={`rotate(${secondRotation}, 50, 50)`}
+          style={{ transition: "transform 0.1s linear" }}
+          ref={secondHand}
+        />
+      </svg>
+    </div>
+  );
+}
+
+function Input() {
+  "use before interactive";
+
+  const [value, setValue] = $useState(() => {
+    if (typeof window !== "undefined") {
+      const url = new URL(window.location.href);
+      return url.searchParams.get("value") ?? "";
+    }
+    return "";
+  });
+  const input = $ref<HTMLInputElement>(null);
+
+  $pre(({ listenUntilHydrated }) => {
+    if (input.current) {
+      const i = input.current;
+      i.value = value;
+      listenUntilHydrated(input.current, "input", () => {
+        window.history.pushState(
+          {},
+          "",
+          `${window.location.pathname}?value=${i.value}`
+        );
+        setValue(i.value);
+      });
+      i.focus();
+    }
+  });
+
+  return (
+    <div>
+      <input
+        ref={input}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+}
